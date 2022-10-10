7a145f9d557cb7eb4628deacaa91ee07374f8269
PontusDejoung <pontusdejounge@gmail.com>
2022-10-10 13:53:27 +0200

    c

---
+++ main.py
@@ -0,0 +1,28 @@
+allGoods = []
+with open("products.txt") as file:
+    for line in file:
+        parts = line.split(";")
+        product = Product(parts[1],float(parts[2]),parts[0])
+def PrintMenu()->int:
+    print("1. Ny kund")
+    print("2. Avsluta")
+    selectionInMenu = Felhantering((":"),minValue=1,maxValue=2)
+    return selectionInMenu
+def Felhantering(prompt,minValue:int, maxValue:int)->int:
+    while True:
+        try:
+            selection = int(input(prompt))
+            if selection < minValue or selection > maxValue:
+                 print(f"Mata in ett tal mellan {minValue} och {maxValue} tack")
+            else:
+                 break
+        except ValueError:
+             print("Mata in ett tal tack")
+             continue
+    return selection
+while True:
+    selection = PrintMenu()
+    if selection == 1:
+        print("Hej")
+    elif selection == 2:
+        break
