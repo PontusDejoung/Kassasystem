17929aafd4d89b12b104ff130601d3166df98636
PontusDejoung <pontusdejounge@gmail.com>
2022-10-12 08:25:37 +0200

    a

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ with open("products.txt") as file:
         parts = line.split(";")
         product = Product(parts[1],float(parts[2]),parts[0])
 def PrintMenu()->int:
+    print("0. Admin")
     print("1. Ny kund")
     print("2. Avsluta")
-    selectionInMenu = Felhantering((":"),minValue=1,maxValue=2)
+    selectionInMenu = Felhantering((":"),minValue=0,maxValue=2)
     return selectionInMenu
 def Felhantering(prompt,minValue:int, maxValue:int)->int:
     while True:
@@ -22,6 +23,8 @@ def Felhantering(prompt,minValue:int, maxValue:int)->int:
     return selection
 while True:
     selection = PrintMenu()
+    if selection == 0:
+        print("a")
     if selection == 1:
         print("Hej")
     elif selection == 2:
