c944c48f3f000fcbc3c1883864f73fb5f794b8a1
PontusDejoung <pontusdejounge@gmail.com>
2022-10-25 21:41:54 +0200

    a

--- receipt.py
+++ receipt.py
@@ -1,21 +1,48 @@
+from ast import Add
 from datetime import datetime
+from typing import Counter
+from product import Product
 
 
 class ReceiptRow:
-    def __init__(self):
-        self.__ProductName = ""
-        self.__Count = 0
-        self.__PerPrice = 0
+    def __init__(self,productName, count, perPrice):
+        self.__ProductName = productName
+        self.__Count = count
+        self.__PerPrice = perPrice
+    def AddCount(self,count):
+        self.__Count = self.__Count + count
     def GetTotal(self):
-        return self.__Count * self.__Price
-
-
+        return self.__Count * self.__PerPrice
+    def GetName(self):
+        return self.__ProductName
+    def GetCount(self):
+        return self.__Count
+    def GetPerPrice(self):
+        return self.__PerPrice
+# jag borde skapa GetReceiptRowTotal(self) get total
 class Receipt:
     def __init__(self):
-        self.__Datum = datetime.now()
+        self.__Datum = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         self.__ReceiptRows = []
-    def GetTotal(self):
+    def GetTotalSum(self):
         sum = 0
         for row in self.__ReceiptRows:
             sum = sum + row.GetTotal()
             return sum
+    def GetDate(self):
+        return self.__Datum
+    def ADD(self,productName:str, count:int, perPrice:float):
+        #Finns redan en receiptrow me ddenna productName
+        # loopa igenom self.__ReceiptRows och försöka hitta
+        # rec.AddcCount
+        #ja -> uspdatera count
+        receiptRow = ReceiptRow(productName,count,perPrice)
+        for prduct in self.__ReceiptRows:
+            if prduct.GetName() == receiptRow.GetName():
+                receiptRow.AddCount(count)
+                return
+        self.__ReceiptRows.append(receiptRow)
+    def GetReceiptRows(self,productname:str):
+        for x in self.__ReceiptRows:
+            if x.GetName() == productname:
+                return x.GetName(),x.GetCount(),x.GetPerPrice()
