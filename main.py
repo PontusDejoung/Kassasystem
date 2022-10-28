fd9ffff80f9cc5fae8579cbaeb8925a5ff7ba1b1
PontusDejoung <pontusdejounge@gmail.com>
2022-10-28 15:37:50 +0200

    change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from re import A
+from time import strftime
 from product import Product
 from receipt import Receipt, ReceiptRow
-from datetime import datetime
+from datetime import date, datetime
 
 
 
@@ -24,8 +26,41 @@ def Felhantering(prompt,minValue:int, maxValue:int)->int:
              continue
     return selection
 
-def Admin():
-    pass
+def AdminMenu():
+    print(f"1. Sök kvitton")
+    selectionInAdminMenu = Felhantering((":"),minValue=0,maxValue=2)
+    return selectionInAdminMenu
+def AdminMenuSelection():
+    while True:
+        selectionAdminMenu = AdminMenu()
+        if selectionAdminMenu == 1:
+            receiptSearch()
+def receiptSearch():
+        inputDate = input("Skriv in datum i format YYYY-MM-DD")
+        try:
+            time = datetime.strptime(inputDate, "%Y-%m-%d")
+        except ValueError:
+            print("Fel datum format, formatet ska vara (yyyymmdd)")
+        #if time != datetime.strptime("%Y-&m-%d"): #len(inputDate) < 8 or inputDate.isnumeric() == False:
+           #print("Fel datum format, formatet ska vara (yyyymmdd)")
+        # file_located = listSpecReceipt(input_str)
+        # current_file = FileOpen(f"{file_located}")
+        with open("receipts.txt","r") as file:
+            for lines in file:
+                if lines.startswith("Kvitto:"):
+                    parts = lines.split(" ")
+                    splitparts = str(parts[2])
+                    split = datetime.strptime(splitparts, "%Y-&m-%d-%H:%M:%S")
+                    if time == split.date:
+                        print(lines)
+                if lines.startswith("Kvitto:") or lines.startswith("Total:"):
+                    print(lines)
+            print("Hittade inte någgot kvitto på den dagen")
+        fullreceipt = input("Vill du se fullständigt kvitto från alla den dagen? Ja/Nej: ").lower()
+        if fullreceipt[0] == "j" and len(fullreceipt) == 2:
+            with open("receipts.txt","r") as file:
+                for lines in file:
+                    print(lines)
 def findProducts(productid):
     for x in allGoods:
         if x.GetProductId() == productid:
@@ -42,10 +77,10 @@ def splitParts():
 def SaveReceiptToFile(findReceiptRow,receipt):
     receiptNumber = FindReceiptNumber()
     with open("receipts.txt","a") as file:
-        file.write(f"Kvitto {receiptNumber} {receipt.GetDate()}\n")
+        file.write(f"Kvitto: {receiptNumber} {receipt.GetDate()} ")
         for row in findReceiptRow:
-            file.write(f"{row.GetName()} {row.GetCount()} * {row.GetPerPrice()} = {row.GetTotal()}\n")
-        file.write(f"Total:{receipt.GetTotalSum()}\n\n")
+            file.write(f"\n{row.GetName()} {row.GetCount()} * {row.GetPerPrice()} = {row.GetTotal()}")
+        file.write(f"\nTotal:{receipt.GetTotalSum()}")
 def FindReceiptNumber():
     with open("receiptnumber.txt", "r") as file:
         number = file.readline()
@@ -53,7 +88,7 @@ def FindReceiptNumber():
         with open("receiptnumber.txt", "w") as file:
             file.write(str(newNumber))
     return number
-def PrintShopingCart(receipt,findReceiptRow):
+def PrintReceipt(receipt,findReceiptRow):
         print(f"Kvitto {receipt.GetDate()}")
         for row in findReceiptRow:
             print(f"{row.GetName()} {row.GetCount()} * {row.GetPerPrice()} = {row.GetTotal()}")
@@ -89,11 +124,11 @@ def NewReceipt():
                 belopp = findProduct[1]
                 receipt.ADD(namn,int(antal),float(belopp))
                 findReceiptRow = receipt.GetReceiptRows()
-                PrintShopingCart(receipt,findReceiptRow)
+                PrintReceipt(receipt,findReceiptRow)
 while True:
     selection = PrintMenu()
     if selection == 0:
-        Admin()
+        AdminMenuSelection()
     elif selection == 1:
         NewReceipt()
     elif selection == 2:
